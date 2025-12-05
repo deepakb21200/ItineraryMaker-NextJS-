652c984fc47aaa0af265211405239bb910e6bc1f
Deepak <deepakbisht17000@gmail.com>
2025-12-05 22:35:17 +0530

    complete

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,7 +1,14 @@
 import {createClient} from "@supabase/supabase-js"
 
 
+// export let supabase = createClient(
+//     "https://ijdhrgffiqfyigpgbggd.supabase.co",
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZGhyZ2ZmaXFmeWlncGdiZ2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NzM3MzIsImV4cCI6MjA4MDI0OTczMn0.97W6qLg1K72TsW39afZ4WgYlo5QJR6snGUC8We-ZzL4"
+// )
+
+
+
 export let supabase = createClient(
-    "https://ijdhrgffiqfyigpgbggd.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqZGhyZ2ZmaXFmeWlncGdiZ2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2NzM3MzIsImV4cCI6MjA4MDI0OTczMn0.97W6qLg1K72TsW39afZ4WgYlo5QJR6snGUC8We-ZzL4"
+      import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
 )
